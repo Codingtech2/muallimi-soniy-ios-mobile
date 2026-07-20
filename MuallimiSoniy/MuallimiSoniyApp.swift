637e0b93c8f545e3bc98f35d8314657d31faaf7d
Azimjon <76002783+azimxxm@users.noreply.github.com>
2026-07-20 19:08:44 +0500

    Initial Commit

---
+++ MuallimiSoniy/MuallimiSoniyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MuallimiSoniyApp.swift
+//  MuallimiSoniy
+//
+//  Created by Coding Tech on 20/07/26.
+//
+
+import SwiftUI
+
+@main
+struct MuallimiSoniyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
